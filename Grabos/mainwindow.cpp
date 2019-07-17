e6c2dc363d3a06d15c38b5556da05bd9a7fb42b4
Poniav <macadition@gmail.com>
2019-07-17 17:25:59 +0200

    Add leave & open button

--- Grabos/mainwindow.cpp
+++ Grabos/mainwindow.cpp
@@ -12,8 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("TextEditeur");
+    this->setWindowTitle("Grabos");
 
+    connect(ui->pushButton_Quitter, SIGNAL(clicked()), this, SLOT(close()));
     connect(ui->actionQuitter, SIGNAL(triggered()), this, SLOT(close()));
     connect(ui->actionA_propos, SIGNAL(triggered()), this, SLOT(openAction()));
     connect(ui->actionOuvrir, SIGNAL(triggered()), this, SLOT(getFile()));
@@ -27,6 +28,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//void MainWindow::closeWindow()
+//{
+//    close();
+//}
+
+///
+/// \brief MainWindow::openAction
+///
 void MainWindow::openAction()
 {
     fwindow = new Dialog(this);
@@ -60,14 +69,14 @@ void MainWindow::getFile()
 
 void MainWindow::saveFileOpen()
 {
-//    QString fileName = QFileDialog::getSaveFileName(this, QString("Sauvegarder le fichier"), QDir::homePath(), "Les fichiers (*.txt)");
+    QString fileName = QFileDialog::getSaveFileName(this, QString("Sauvegarder le fichier"), QDir::homePath(), "Les fichiers (*.txt)");
 
-//    QFile file(fileName);
+    QFile file(fileName);
 
-//    if (file.open(QIODevice::WriteOnly))
-//    {
+    if (file.open(QIODevice::WriteOnly))
+    {
 
-//    }
+    }
 
     qDebug() << this->fileName;
 
@@ -79,11 +88,19 @@ void MainWindow::closeFileOpened()
     this->setFileName("");
 }
 
+///
+/// \brief MainWindow::getFileName
+/// \return QString filename
+///
 QString MainWindow::getFileName()
 {
     return this->fileName;
 }
 
+///
+/// \brief MainWindow::setFileName
+/// \param file
+///
 void MainWindow::setFileName(QString file)
 {
     this->fileName = file;
